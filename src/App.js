f27101171647ff26f29cbe363087cf498f9f6f2b
Anastasiya <avojtesik1@gmail.com>
2022-02-04 20:07:35 +0300

    add a part in one click

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ScreenRecording from './components/screenRecording/screenRecording';
+import InOneClick from './components/inOneClick/inOneClick';
 
 function App() {
   return (
     <>
   <ScreenRecording />
+  <InOneClick/>
     </>
   );
 }
