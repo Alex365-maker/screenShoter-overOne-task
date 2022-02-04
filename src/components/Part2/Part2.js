dfe8174aa2a85898482af2e77ee568c0a1da7c15
Alex365-maker <70705449+Alex365-maker@users.noreply.github.com>
2022-02-05 01:36:21 +0300

    add part 2

--- src/components/Part2/Part2.js
+++ src/components/Part2/Part2.js
@@ -1,10 +1,31 @@
 import './stylesPart2.css';
-//import logo from './images/logo.svg'
+import img1 from './images/img1.svg'
+import img2 from './images/img2.svg'
+import img3 from './images/img3.svg'
+import img4 from './images/img4.svg'
 
 const Part2 = () => {
   return (
    <>
-   
+   <div className='Part2Container'>
+<h2>Встречайте — скриншоты и запись экрана <span>2 в 1</span></h2>
+<span>Больше не нужно искать две отдельные программы для скриншотов и записи экрана. Screenshoter поможет сделать <strong>снимок экрана, записать видео и поделиться</strong> им <strong>с кем угодно</strong>. Можно выделить весь экран, определенную область или активное окно</span>
+   </div>
+   <div className='CardsContainer'>
+       <div>
+           <img src={img1} />
+           <h4>Снимок и запись экрана в 1 клик</h4>
+           </div>
+       <div>
+       <img src={img2} />
+       <h4>Моментальная ссылка на файл</h4></div>
+       <div>
+       <img src={img3} />
+       <h4>Удобный редактор снимков</h4></div>
+       <div>
+       <img src={img4} />
+       <h4>Бесплатно и без регистрации</h4></div>
+   </div>
    </>
   );
 }
