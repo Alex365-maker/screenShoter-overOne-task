b0e740a72d34dd8d5ef0b2c0241866cce878cb7a
Anastasiya <avojtesik1@gmail.com>
2022-02-05 17:25:47 +0300

    add part photo editor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import ScreenRecording from './components/screenRecording/screenRecording';
 import InOneClick from './components/inOneClick/inOneClick';
 import InstantLink from './components/instantLink/instantLink';
+import PhotoEditor from './components/photoEditor/photoEditor';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
   <ScreenRecording />
   <InOneClick/>
   <InstantLink/>
+  <PhotoEditor/>
     </>
   );
 }
